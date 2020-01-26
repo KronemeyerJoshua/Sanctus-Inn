47f7f36f5a8232ae0d24f85fad56149b93a466ed
KronemeyerJoshua <jkroneme@asu.edu>
2020-01-26 11:23:47 -0700

    Changed formatting to reflect new request information

--- resources/js/components/forum/ThreadPreview.vue
+++ resources/js/components/forum/ThreadPreview.vue
@@ -1,22 +1,16 @@
 <template>
-    <div class="thread">
-        <div class="threadTitle">
-            <router-link :to="{ name : 'threadPreview', params: { threadId: this.id } }"><slot name="title"></slot></router-link>
-        </div>
-        <div class="threadDescription">
-            <slot name="desc"></slot>
-        </div>
-        <div class="meta-info">
-            <div class="threadCount">
-
-            </div>
-            <div class="commentCount">
-
-            </div>
-            <div class="mostRecent">
-
+    <div class="columns is-mobile">
+        <div class="column is-three-fifths">
+            <div class="categoryTitle">
+                <router-link :to="{ name : 'threadPreview', params: { threadId: this.id } }"><slot name="title"></slot></router-link>
             </div>
         </div>
+            <div class="commentCount column is-one-fifth">
+                <slot name="comments"></slot>
+            </div>
+            <div class="mostRecent column is-one-fifth">
+                <slot name="mostRecent"></slot>
+            </div>
     </div>
 </template>
 
@@ -30,5 +24,4 @@
 </script>
 
 <style scoped>
-
 </style>
