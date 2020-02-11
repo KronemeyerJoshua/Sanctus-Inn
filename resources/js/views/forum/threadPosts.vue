d1893f47c40f82c50bde20848f1a955f1964c00f
KronemeyerJoshua <jkroneme@asu.edu>
2020-02-11 16:23:31 -0700

    WYSIWYG Editor Implementation for post reply

--- resources/js/views/forum/threadPosts.vue
+++ resources/js/views/forum/threadPosts.vue
@@ -1,17 +1,22 @@
 <template>
     <div>
-<Post v-for="post in posts" :key="post.id">
-    <template v-slot:avatar><img src="https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy80MTQyNjY5L29yaWdpbi5qcGciLCJleHBpcmVzX2F0IjoxNTgwNDMzNjcyfQ.na9LHtGFnpQGv-AtAgl9c84BkPaAYICRVmnancQL_qU/img.jpg?width=980"></template>
-    <template v-slot:username>{{post.user.name}}</template>
-    <template v-slot:timestamp>{{ dateFormat(post.created_at) }}</template>
-    <template v-slot:content>{{ post.content }}</template>
-</Post>
+    <Post v-for="post in posts" :key="post.id">
+        <template v-slot:avatar><img src="https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy80MTQyNjY5L29yaWdpbi5qcGciLCJleHBpcmVzX2F0IjoxNTgwNDMzNjcyfQ.na9LHtGFnpQGv-AtAgl9c84BkPaAYICRVmnancQL_qU/img.jpg?width=980"></template>
+        <template v-slot:username>{{post.user.name}}</template>
+        <template v-slot:timestamp>{{ dateFormat(post.created_at) }}</template>
+        <template v-slot:content>{{ post.content }}</template>
+    </Post>
+        <div>
+            <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+        </div>
     </div>
 </template>
 
 <script>
     import Post from "../../components/forum/Post";
     import { forum } from "../../services/EventService"
+    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
     export default {
         name: "threadPosts",
         components: {
@@ -23,7 +28,12 @@
         },
         data() {
             return {
-                posts: {}
+                posts: {},
+                editor: ClassicEditor,
+                editorData: '<p>Content of the editor.</p>',
+                editorConfig: {
+                    // The configuration of the editor.
+                }
             }
         },
         created() {
