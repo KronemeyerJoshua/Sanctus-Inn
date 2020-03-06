70e383a62b6d51d78d0b9da1dd6096cc15f91565
KronemeyerJoshua <jkroneme@asu.edu>
2020-03-05 20:21:06 -0700

    API call to get a single profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Profile;
+
 
 class ProfileController extends Controller
 {
-    //
+    public function getProfile($userId = null)
+    {
+        return Profile::where('id', $userId)->first();
+    }
 }
