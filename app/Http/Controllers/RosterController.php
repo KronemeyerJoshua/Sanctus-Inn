d5dc8e9507dbbdaafd3e6109513f833798af52e9
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-05 05:05:26 -0700

    Roster Controller for pulling Discord Members

---
+++ app/Http/Controllers/RosterController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Cache;
+class RosterController
+
+{
+    public function rosterdata(Request $request)
+    {
+        $json_options = [
+            "http" => [
+                "method" => "GET",
+                "header" => "Authorization: Bot NjM2NzkyMjc1NTY1MDE5MTQ3.XbE61A.d6ZwpH7OFdPFbCOtWvdCIXjxMqU"
+            ]
+        ];
+        $json_decode = array();
+        $dbupdate = \DB::table('last_updated')->where('name', 'roster')->value('last_updated');
+        $dbupdate = strtotime($dbupdate);
+        if (Cache::has('roster')) {
+            return Cache::get('roster');
+        } else {
+            $game_races = array("Kaelar", "Vaelune", "Empyrean", "Py'Rai", "Ren'Kai", "Vek", "Dünir", "Niküa", "Tulnar");
+            $game_classes = array("Bard", "Cleric", "Fighter", "Mage", "Ranger", "Rogue", "Summoner", "Tank");
+
+            $json_context = stream_context_create($json_options);
+
+            $json_get = file_get_contents('https://discordapp.com/api/guilds/504797119530663964/roles?limit=1000', false, $json_context);
+
+            $roles = array();
+
+            $json_decode = json_decode($json_get, true);
+
+            foreach ($json_decode as $role) {
+                $roles += [$role['id'] => $role['name']];
+            }
+
+            $json_get = file_get_contents('https://discordapp.com/api/guilds/504797119530663964/members?limit=1000', false, $json_context);
+
+            $json_decode = json_decode($json_get, true);
+
+            $users = array();
+            foreach ($json_decode as $member) {
+                if (in_array("637060125420552209", $member['roles'])) {
+                    $user = array();
+                    $user['id'] = $member['user']['id'];
+                    if (is_null($member['nick']))
+                        $user['name'] = $member['user']['username'];
+                    else
+                        $user['name'] = $member['nick'];
+
+                    foreach ($member['roles'] as $role) {
+                        if (in_array($roles[$role], $game_races))
+                            $user['race'] = $roles[$role];
+
+                        foreach ($member['roles'] as $role) {
+                            if (in_array($roles[$role], $game_classes)) {
+                                {
+                                    $user['class'] = $roles[$role];
+                                }
+                            }
+
+                        }
+                    }
+
+                    \DB::table('discord_roster')->updateOrInsert([
+                        'id' => $user['id'],
+                    ], $user);
+                }
+
+            }
+            $json_decode = \DB::table('discord_roster')->select(array('name', 'race', 'class'))->get();
+            Cache::add('roster', $json_decode, now()->addHours('12'));
+        }
+
+        return $json_decode;
+    }
+}
