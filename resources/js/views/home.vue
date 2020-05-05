7bde1925787ec2f54f6f1be691dd9604305d36f1
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-05 08:05:26 -0700

    Updated newspage template to include bignewscard component

--- resources/js/views/home.vue
+++ resources/js/views/home.vue
@@ -1,32 +1,22 @@
 <template>
     <div class="columns is-multiline">
         <div class="column is-two-thirds">
-            <NewsCard v-for="count in counter" style="margin-bottom: 15px;" :key="count"></NewsCard>
+            <div class="columns is-multiline is-centered">
+                <BigNewsCard v-for="count in counter" class="column" style="" :key="count"></BigNewsCard>
+            </div>
         </div>
 
         <div class="column is-one-third">
-            <div style="height: 200px; border: solid 1px white; margin-bottom: 15px;" >
-                <div style="border-bottom: solid 1px white; text-align: center;" >
-                    <p style="font-weight: bold; color: rgba(255, 255, 255, 0.7);">Live Now</p>
-                </div>
-                <div>
-                    TwitchPlayer
-                </div>
-            </div>
-            <div style="height: 60px; border: solid 1px white;" >
-                <div style="border-bottom: solid 1px white; text-align: center;" >
-                    <p style="font-weight: bold; color: rgba(255, 255, 255, 0.7);">Guild Node Progression</p>
-                </div>
-                <div style="padding: 10px;">
-                    <progress class="progress" value="15" max="100">15%</progress>
-                </div>
-            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import BigNewsCard from "../components/BigNewsCard";
     export default {
+        components: {
+            BigNewsCard
+        },
         data() {
             return {
                 counter: 3,
@@ -36,3 +26,6 @@
         }
     }
 </script>
+
+<style scoped>
+</style>
