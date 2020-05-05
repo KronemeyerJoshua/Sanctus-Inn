b572bb3a50d9f49fe8a553b721da8ba6e7caf2c6
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-05 08:03:35 -0700

    Removed extra div

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,7 +1,6 @@
 <template>
     <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div id="navbar" class="navbar-menu">
-            <div class="navbar-start" style="flex-grow: 1; justify-content: center">
+        <div id="navbar" class="navbar-menu" style="flex-grow: 1; justify-content: center;">
                 <router-link :to="{ name : 'about-us' }" class="navbar-item">
                     About
                 </router-link>
@@ -14,8 +13,8 @@
                     Live
                 </router-link>
 
-                <router-link :to="{ name : 'home' }">
-                    <img src="/images/logo.png" alt="" style="">
+                <router-link :to="{ name : 'home' }" class="nav-image-parent">
+                    <img src="/images/logo.png" alt="" class="nav-image">
                 </router-link>
 
                 <router-link :to="{ name : 'apply' }" class="navbar-item">
@@ -27,7 +26,7 @@
                 </router-link>
 
                 <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
+                    <a class="navbar-link is-arrowless">
                         Tools
                     </a>
 
@@ -37,13 +36,13 @@
                         </a>
                     </div>
                 </div>
-            </div>
+        </div>
 
-            <div class="navbar-end" style="float: right; width: 0;">
+            <div class="navbar-end" style="width: 0px;">
                 <div class="navbar-item has-dropdown is-hoverable" v-if="userexists">
 
                     <a class="navbar-link">
-                        {{username}}
+                        <router-link :to="'/profile/' + userid" class="navbar-item">{{username}}</router-link>
                     </a>
 
                     <div class="navbar-dropdown">
@@ -55,14 +54,13 @@
 
                 <div class="navbar-item" v-else-if="!userexists">
                     <div class="buttons">
-                        <button @click="showRegister" class="button is-uppercase">
+                        <button @click="showRegister" class="button is-uppercase is-small">
                             <strong>Sign up</strong>
                         </button>
-                        <button @click="showLogin" class="button is-uppercase"><strong>Login</strong></button>
+                        <button @click="showLogin" class="button is-uppercase is-small"><strong>Login</strong></button>
                     </div>
                 </div>
             </div>
-        </div>
     </nav>
 </template>
 
@@ -104,6 +102,13 @@ export default {
                 {
                 }
         },
+        userid() {
+            try {
+                return this.$store.state.auth.user.id;
+            } catch (e)
+            {
+            }
+        },
     },
     created() {
     }
@@ -112,36 +117,76 @@ export default {
 
 <style scoped>
     .navbar {
-        background: transparent;
-        border-bottom: 1px solid white;
-        backdrop-filter: blur(8px);
+        background: rgba(0,0,0,0.3);
+        border-bottom: 1px solid rgba(255,255,255,0.1);
+        -webkit-box-shadow: 0 8px 6px -6px #000;
+        -moz-box-shadow: 0 8px 6px -6px #000;
+        box-shadow: 0 8px 6px -6px #000;
     }
-
+    .button
+    {
+        background: transparent;
+        border-color: rgb(133,207,170);
+        color: rgba(255,255,255,0.7);
+    }
+    .nav-image
+    {
+        top: 2px;
+        opacity: 0.7;
+        position: absolute;
+    }
+    .nav-image-parent
+    {
+        width: 48px;
+    }
+    .nav-image:hover
+    {
+        opacity: 1;
+    }
     .navbar-item, .navbar-link {
-        font-size: 22px;
+        font-size: 24px;
         text-transform: uppercase;
         font-weight: bold;
-        font-family: 'IM Fell Great Primer SC', serif;
-        color: rgba(255, 255, 255, 0.7)
+        font-family: 'EB Garamond', serif;
+        color: rgba(150, 150, 150, 0.7);
+        text-shadow: black 0px 0px 4px;
+        padding-top: 0px;
+        padding-bottom: 0px;
+        transition: linear 0.2s;
     }
 
     .navbar-item:hover, .navbar-link:hover, .has-dropdown:hover, .navbar-item.has-dropdown:focus .navbar-link, .navbar-item.has-dropdown:hover .navbar-link, .navbar-item.has-dropdown.is-active .navbar-link {
         background: none !important;
     }
 
-    .navbar-link:hover, .navbar-dropdown a.navbar-item:hover {
-        color: #3490dc;
-    }
+    .navbar-link:hover, a.navbar-item:hover, .navbar-dropdown a.navbar-item:hover  {
 
+        color: #bd8647;
+        text-shadow: #eea756 0 0 10px;
+    }
+    .router-link-exact-active, .router-link-active, .router-link-exact-active .nav-image
+    {
+        opacity: 1;
+        color: #bd8647 !important;
+        text-shadow: #eea756 0 0 10px;
+    }
     .navbar-dropdown {
-        background: rgba(10,2,59,0.9);
+        background: rgba(0,0,0,0.4);
         backdrop-filter: blur(8px);
-        border: white 1px solid;
+        border: 1px solid rgba(255,255,255,0.1);
     }
     a.navbar-item:focus, a.navbar-item:focus-within, a.navbar-item:hover, a.navbar-item.is-active, .navbar-link:focus, .navbar-link:focus-within, .navbar-link:hover, .navbar-link.is-active {
         background: none;
     }
     .router-link-active {
         color: #3490dc;
+    }
+    @media screen and (min-width: 1025px) {
+        .navbar-item.is-hoverable:hover .navbar-dropdown {
+            display: block !important;
+        }
+        .navbar-item.is-hoverable:focus-within .navbar-dropdown {
+            display: none;
+        }
     }
 </style>
