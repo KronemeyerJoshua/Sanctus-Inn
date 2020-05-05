32e5cc9a3c5d2db2a8b1c919a943a9acda45c82a
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-05 05:57:34 -0700

    First pass on BigNewsCard component

---
+++ resources/js/components/BigNewsCard.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="card">
+            <p class="headline title is-4 is-flex"><router-link :to="{ name : 'home' }">Evolution Guild Site Live! Evolution Guild Site Live!</router-link></p>
+            <p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        <div class="bottom-card is-flex" style="justify-content: space-between">
+            <p><router-link to="/forum/1">Announcements</router-link> by <router-link to="/profile/1">Gubstep</router-link></p>
+            <p style="">3/7/2019 2:23:13</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .bottom-card p a {
+        color: #4482e5;
+    }
+    .card a
+    {
+        color: rgba(240,240,240,0.9);
+    }
+    .card a:hover
+    {
+        text-shadow: #bd8647 0px 0px 6px;
+    }
+    .card {
+        background: rgba(0,0,0,0.7);
+        text-align: center;
+        color: rgba(255,255,255,0.8);
+        min-height: 250px;
+        max-height: 250px;
+        min-width: 100%;
+        max-width: 100%;
+        margin: 10px;
+        box-shadow: #0a0a0a 1px 1px 2px 2px;
+        border: 1px solid rgba(0,0,0,0.5);
+        border-radius: 5px;
+        padding: 0;
+        overflow: hidden;
+        transition: linear 0.2s;
+        font-family: "Roboto",sans-serif;
+    }
+    .card:hover
+    {
+        box-shadow: #bd8647 1px 1px 8px 2px;
+    }
+    .headline a
+    {
+        color: rgba(240,240,240,0.9);
+        text-shadow: 1px 1px 1px #000;
+    }
+    .headline {
+        text-align: center;
+        position: relative;
+        padding: 5px;
+        background: no-repeat linear-gradient( rgba(10, 0, 0, 0.2), rgba(10, 0, 0, 0.2) ), url('https://happygamer.com/wp-content/uploads/2019/09/Ashes-of-Creation.jpg');
+        background-size: cover;
+        background-position: center;
+        height: 60px;
+        width: 100%;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 0;
+        font-family: 'EB Garamond', serif;
+    }
+    .content
+    {
+        padding: 10px 10px 0px 10px;
+        overflow: hidden;
+        display: -webkit-box;
+        -webkit-line-clamp: 6;
+        -webkit-box-orient: vertical;
+        margin-bottom: 0.5rem;
+    }
+    .bottom-card
+    {
+        padding-left: 10px;
+        padding-right: 10px;
+    }
+</style>
