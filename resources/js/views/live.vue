81a6466e1b36411a03a873148edafc03ac7b04d0
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-06 05:35:27 -0700

    Updated tabs, changed axios to event service for tab calls

--- resources/js/views/live.vue
+++ resources/js/views/live.vue
@@ -33,9 +33,11 @@
                 </div>
 
                 <div class="columns is-marginless">
-                    <div class="column" @click="{ loadgame(510699); activetab = 0; }" :class="{'active' : activetab === 0}">APOC</div>
-                    <div class="column" @click="{ loadgame(138585); activetab = 1; }" :class="{'active' : activetab === 1}">Ashes of Creation</div>
-                    <div class="column" @click="{ loadguild(); activetab = 2; }" :class="{'active' : activetab === 2}">Guild</div>
+                    <!-- Temp Switched to Hearthstone / WoW -->
+                    <!-- APOC 510699 | Ashes of Creation 138585 -->
+                    <div class="column is-tab" @click="{ loadgame(138585); activetab = 0; }" :class="{'active' : activetab === 0}">HearthStone</div>
+                    <div class="column is-tab" @click="{ loadgame(18122); activetab = 1; }" :class="{'active' : activetab === 1}">World of Warcraft</div>
+                    <div class="column is-tab" @click="{ loadguild(); activetab = 2; }" :class="{'active' : activetab === 2}">Guild</div>
                 </div>
                 <div class="columns is-multiline" key="3" style="padding-top: 10px;">
                     <a v-for="user in twitchdata.twitch.data" @click="loadIframe(user.user_id)"  class="column is-one-quarter" style="overflow: hidden;">
@@ -56,6 +58,31 @@
     </div>
 
 </template>
+
+<style>
+    .titlebar {
+        color: rgba(255,255,255,0.7)
+    }
+    .viewers {
+        color: rgba(255,255,255,0.7)
+    }
+    .is-tab {
+        border: 1px solid white;
+        text-align: center;
+        color: rgba(255,255,255,0.7);
+        font-weight: bold;
+    }
+    .active {
+        color: #bd8647;
+        text-shadow: #eea756 0 0 10px;
+        background: rgba(0,0,0,0.4);
+    }
+    .is-tab:hover {
+        color: #bd8647;
+        text-shadow: #eea756 0 0 10px;
+        background: rgba(0,0,0,0.2);
+    }
+</style>
 
 <script>
     import { live } from "../services/EventService"
@@ -85,12 +112,12 @@
                 this.iframeLoaded = true;
             },
             loadgame: function(gameid) {
-                axios.get('/api/twitchdata/' + gameid)
-                    .then(({data}) => this.twitchdata = data );
+                live.getStreamsByGameId(gameid)
+                    .then(({data}) => { this.twitchdata = data;
+                    console.log(data);
+                    } );
             },
             loadguild: function() {
-                axios.get('/api/twitchuser/arustedknight')
-                    .then(({data}) => this.twitchdata = data );
             }
         },
         created() {
