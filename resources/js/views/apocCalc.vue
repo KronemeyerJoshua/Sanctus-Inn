b1be84c3fb7ae396be0cf18e706b7bddfa5bd0fc
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-06 05:33:22 -0700

    Added ApocCalc component to page

--- resources/js/views/apocCalc.vue
+++ resources/js/views/apocCalc.vue
@@ -1,8 +1,12 @@
 <template>
+    <ApocCalc></ApocCalc>
 </template>
 
 <script>
+    import ApocCalc from "../components/calc/ApocCalc";
     export default {
+        name: "apoc-calc",
+        components: {ApocCalc},
         mounted() {
         }
     }
