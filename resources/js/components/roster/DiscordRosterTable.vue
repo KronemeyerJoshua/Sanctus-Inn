1d4dea4c4a0b74b3f46475d328f652465db2eacf
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-08 02:57:28 -0700

    Column sorting implemented, css th fix

--- resources/js/components/roster/DiscordRosterTable.vue
+++ resources/js/components/roster/DiscordRosterTable.vue
@@ -8,19 +8,19 @@
         <table class="table is-hoverable is-fullwidth">
             <thead>
             <tr>
-                <th>Name</th>
-                <th>Race</th>
-                <th>Class</th>
+                <th @click="sortListString('name')">Name</th>
+                <th @click="sortListString('race')">Race</th>
+                <th @click="sortListString('class')">Class</th>
             </tr>
             </thead>
-            <tr v-for="member in roster_json.slice(( (this.currentPage-1) * pageSize), pageSize * currentPage)">
+            <tr v-for="member in this.roster_json.slice(( (this.currentPage-1) * pageSize), pageSize * currentPage)">
                 <td>{{member.name}}</td>
                 <td>{{member.race}}</td>
                 <td>{{member.class}}</td>
             </tr>
         </table>
         <p>
-            <button @click="decrementPage" :disabled="cantGoBack">Previous</button>
+            <button @click="decrementPage" :disabled="this.currentPage === 1">Previous</button>
             <button @click="incrementPage">Next</button>
         </p>
     </div>
@@ -34,12 +34,16 @@
         background: rgba(0,0,0,0.7);
         color: rgba(240,240,240,0.9);
     }
-    tr:hover {
+    .table > tr:hover {
         color: #bd8647;
         text-shadow: #eea756 0 0 10px;
     }
     .table thead th {
         color: rgba(255,255,255,0.7);
+    }
+    .table thead th:hover {
+        color: #bd8647;
+        text-shadow: #eea756 0 0 10px;
     }
 </style>
 
@@ -56,6 +60,7 @@ export default {
                           {field: 'race', label: 'Race', sortable: true},
                           {field: 'class', label: 'Class', sortable: true}],
                 isLoading: true,
+                sortProps: {name: 'name', dir: 'asc', inverse: 1},
                 pageSize: 15,
                 currentPage: 1
             }
@@ -67,21 +72,42 @@ export default {
             },
             decrementPage() {
                 this.currentPage--;
+            },
+            sortListString(columnName) {
+                if (this.sortProps.name != columnName) {
+                    this.sortProps.name = columnName;
+                    this.sortProps.dir = 'asc';
+                    this.sortProps.inverse = 1;
+                } else {
+                    if (this.sortProps.dir == 'asc') {
+                        this.sortProps.dir = 'desc';
+                        this.sortProps.inverse = -1;
+                    } else {
+                        this.sortProps.dir = 'asc';
+                        this.sortProps.inverse = 1;
+                    }
+                }
+
+                const sortP = this.sortProps;
+                this.roster_json.sort(
+                function(a,b) {
+                    a = a[columnName];
+                    b = b[columnName];
+                    if (a < b || b === null) {
+                        return -1 * sortP.inverse;
+                    } else if (a > b || a === null) {
+                        return 1 * sortP.inverse;
+                    } else {
+                        return 0;
+                    }
+                });
             }
         },
-
-    computed: {
-            cantGoBack() {
-                return this.currentPage === 1;
-            },
-
-    },
 
         async created() {
             roster.getRosterData()
                 .then(({data}) => {
                     this.roster_json = data;
-                    console.log(data);
                 })
                 .catch((error) => {
                     console.log(error)
