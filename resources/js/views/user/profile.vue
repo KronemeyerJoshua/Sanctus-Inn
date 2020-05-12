021fc24ece1652d642dc449bc7b2d1fba6dc999e
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-11 19:22:38 -0700

    Messy first pass at resizing before upload to save bandwidth

--- resources/js/views/user/profile.vue
+++ resources/js/views/user/profile.vue
@@ -2,7 +2,12 @@
     <div v-if="loaded" class="profile">
         <div class="title">{{ this.profile.forum_id }}</div>
         <div class="columns">
-            <div class="profile-avatar column is-one-fifth"></div>
+            <div class="profile-avatar column is-one-fifth">
+                <img :src="'/storage/images/' + this.profile.id + '.jpg'">
+                <div v-if="this.$store.state.auth.user.id === this.profile.id">
+                    <input id="uploader" type="file" accept="image/*" style="margin-bottom: 5px;" @change="uploadImage">
+                </div>
+            </div>
             <div class="profile-bio column is-three-fifths"> {{ this.profile.bio }}</div>
             <div class="profile-char column is-one-fifth">
                 <ul>
@@ -27,11 +32,63 @@
         components: {
         },
         methods: {
+            uploadImage(e) {
+                const file = e.target.files[0];
+                let reader = new FileReader();
+                // Create an image
+                reader.onload = (e) => {
+                    var img = document.createElement("img");
+                    img.onload = () => {
+                        var canvas = document.createElement('canvas');
+                        var ctx = canvas.getContext("2d");
+                        ctx.drawImage(img, 0, 0);
+
+                        var MAX_WIDTH = 128;
+                        var MAX_HEIGHT = 128;
+                        var width = img.width;
+                        var height = img.height;
+
+                        if (width > height) {
+                            if (width > MAX_WIDTH) {
+                                height *= MAX_WIDTH / width;
+                                width = MAX_WIDTH;
+                            }
+                        } else {
+                            if (height > MAX_HEIGHT) {
+                                width *= MAX_HEIGHT / height;
+                                height = MAX_HEIGHT;
+                            }
+                        }
+                        canvas.width = width;
+                        canvas.height = height;
+                        var ctx = canvas.getContext("2d");
+                        ctx.drawImage(img, 0, 0, width, height);
+                        let dataUrl = canvas.toDataURL('image/jpeg');
+                        var binary = atob(dataUrl.split(',')[1]);
+                        var array = [];
+                        for(var i = 0; i < binary.length; i++) {
+                            array.push(binary.charCodeAt(i));
+                        }
+
+                        let data = new FormData();
+                        data.append('id', this.$store.state.auth.user.id);
+                        data.append('image', new Blob([new Uint8Array(array)], {type: 'image/jpeg'}));
+                        user.updateProfilePicture(data)
+                            .then(({data}) => console.log(data.message));
+
+                    }
+                    img.src = e.target.result;
+                }
+                // Load files into file reader
+                reader.readAsDataURL(file);
+
+            }
         },
         data() {
             return {
                 profile: null,
-                loaded: false
+                loaded: false,
+                dataUrl: "aiosjnfoijasoidkjmkasmdkl;",
             }
         },
         created() {
