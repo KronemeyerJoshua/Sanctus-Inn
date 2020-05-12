040540044327aaa16a026029d20fa5ad35a2fd54
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-12 09:18:17 -0700

    First pass at bio updating via profile panel

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -18,4 +18,16 @@ class ProfileController extends Controller
         $path = $request->file('image')->storeAs('public/images', $request->user()->id . ".jpg");
         return response()->json(['message' => $path]);
     }
+
+    public function updateUserBio(Request $request)
+    {
+        $user = auth()->user();
+        $profile = $user->profile;
+        $profile->bio = $request->input('bio');
+        if ($profile->save()) {
+            return response()->json(['status' => 200, 'message' => 'Bio has been updated!']);
+        }
+
+        return response()->json(['status' => 400, 'message' => 'Oops! Something went wrong.']);
+    }
 }
