7f2f80f9bf5e30a637d0e7bc60b2a3964930a90a
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-13 06:06:10 -0700

    Added proper links for user profiles, moved some styling into threadPreview component

--- resources/js/views/forum/threads.vue
+++ resources/js/views/forum/threads.vue
@@ -4,7 +4,7 @@
         <section class="forumContainer">
             <div class="forumHeader columns is-mobile">
                 <div class="column is-three-fifths">Title</div>
-                <div class="column is-one-fifth">Discussions</div>
+                <div class="column is-one-fifth has-text-centered">Discussions</div>
                 <div class="column is-one-fifth">Recent</div>
             </div>
             <div class="thread" v-for="thread in threads" :key="thread.id">
@@ -16,8 +16,13 @@
                         {{ thread.posts_count }}
                     </template>
                     <template v-slot:mostRecent>
-
-                        {{ thread.latestpost | checkUserExists }}
+                        <div v-if="userExists">
+                            <router-link :to="'/profile/' + thread.latestpost.user.id">{{thread.latestpost.user.name}}</router-link>
+                            <div>{{ formatDate(thread.latestpost.updated_at)}}</div>
+                        </div>
+                        <div v-else>
+                            No replies
+                        </div>
                     </template>
                 </ThreadPreview>
             </div>
@@ -28,6 +33,7 @@
 <script>
     import ThreadPreview from "../../components/forum/ThreadPreview";
     import { forum } from "../../services/EventService"
+    import moment from 'moment'
     export default {
         name: "threads",
         components: {ThreadPreview},
@@ -44,10 +50,14 @@
                     console.log(error)
                 })
         },
-        filters: {
-            checkUserExists: function(value) {
-                if (!value) return "No replies";
-                return value.user.name + " @ " + value.updated_at;
+        computed: {
+            userExists(value) {
+                return value;
+            }
+        },
+        methods: {
+            formatDate(date) {
+                return moment(date).format('llll')
             }
         }
     }
@@ -55,18 +65,23 @@
 
 <style scoped>
     .threadContainer {
-        padding: 5px;
-        color: white;
-        background: black;
+        padding: 10px;
+        color: rgba(215,215,215,0.9);
+        background: rgba(73, 47, 19, 0.6);
+        box-shadow: #0a0a0a 1px 1px 2px 2px;
+        width: 100%;
+        max-width: 100%;
     }
     .categoryContainer {
         border: 1px solid white;
     }
-    .categoryContainer > .thread:last-child {
+    .thread {
+        border-bottom: 1px solid white;
+    }
+    .forumContainer > .thread:last-child {
         border: none;
     }
-    .thread {
-        height: 80px;
-        border-bottom: 1px solid white;
+    .forumHeader {
+        margin-bottom: 0;
     }
 </style>
