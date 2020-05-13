131abd1d08a597ffa88de53dcfb50a9549301a40
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-13 06:05:02 -0700

    Minor styling changes, fixed a couple width issue bugs

--- resources/js/components/forum/ThreadPreview.vue
+++ resources/js/components/forum/ThreadPreview.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="columns is-mobile">
+    <div class="columns is-desktop is-vcentered single-thread">
         <div class="column is-three-fifths">
             <div class="categoryTitle">
                 <router-link :to="{ name : 'threadPreview', params: { threadId: this.id } }"><slot name="title"></slot></router-link>
             </div>
         </div>
-            <div class="commentCount column is-one-fifth">
+            <div class="commentCount column is-one-fifth has-text-centered">
                 <slot name="comments"></slot>
             </div>
             <div class="mostRecent column is-one-fifth">
@@ -24,4 +24,8 @@
 </script>
 
 <style scoped>
+    .single-thread {
+        height: 90px;
+
+    }
 </style>
