6cb03a62f1c9bf4f368068670a7261e81351f20b
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-13 04:01:59 -0700

    Updated styling and format of post display

--- resources/js/components/forum/Post.vue
+++ resources/js/components/forum/Post.vue
@@ -1,14 +1,18 @@
 <template>
     <article class="media">
         <figure class="media-left">
-            <p class="image is-64x64"><slot name="avatar"><img src="public/images/test-news-image.jpg"></slot></p>
+            <p class="avatar"><slot name="avatar"><img src="public/images/test-news-image.jpg"></slot></p>
+            <strong><slot name="username" class="username">John Smith</slot></strong>
+            <slot name="replyTime"></slot>
         </figure>
         <div class="media-content">
             <div class="content">
                 <p>
-                    <strong><slot name="username">John Smith</slot></strong> <small><slot name="timestamp">20m</slot></small>
                     <br>
-                    <slot name="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</slot>
+                    <slot name="content" class="postBody">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</slot>
+                </p>
+                <p>
+                    <small><slot name="timestamp">20m</slot></small>
                 </p>
             </div>
             <nav class="level is-mobile">
@@ -26,7 +30,7 @@
             </nav>
         </div>
         <div class="media-right">
-            <button class="delete"></button>
+
         </div>
     </article>
 </template>
@@ -41,5 +45,26 @@
 </script>
 
 <style scoped>
-
+    .media-content {
+        width: 80%;
+    }
+    .avatar img {
+        max-height: 64px;
+        height: 64px;
+        width: auto;
+    }
+    .avatar {
+        text-align: center;
+        position: relative;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+    .media-left {
+        width: 232px;
+        max-width: 232px;
+    }
+    .media {
+        text-align: center !important;
+    }
 </style>
