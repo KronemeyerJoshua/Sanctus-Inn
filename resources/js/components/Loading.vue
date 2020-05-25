a198fccd8c1a37e2d23152041a6f9f7b24ed209c
KronemeyerJoshua <jkroneme@asu.edu>
2020-05-25 14:39:18 -0700

    New loading module for pages

---
+++ resources/js/components/Loading.vue
@@ -0,0 +1,33 @@
+<template>
+    <div style="height: 0;">
+        <h3 class="title loading">Going through the divine gateway to get information</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Loading"
+    }
+</script>
+
+<style scoped>
+    .title {
+        text-align: center;
+        padding-top: 10%;
+        color: rgba(212,212,212,0.8);
+    }
+
+    .loading:after {
+        display: inline-block;
+        animation: dotty steps(1,end) 1s infinite;
+        content: '';
+    }
+
+    @keyframes dotty {
+        0%   { content: ''; }
+        25%  { content: '.'; }
+        50%  { content: '..'; }
+        75%  { content: '...'; }
+        100% { content: ''; }
+    }
+</style>
