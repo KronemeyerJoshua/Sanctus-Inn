3181aa7926160fa436a094d283fc00e3b677f299
KronemeyerJoshua <jkroneme@asu.edu>
2020-06-09 06:43:59 -0700

    Moved navburger logic to mounted to fix navburger race condition

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -10,7 +10,7 @@
                 <span aria-hidden="true"></span>
             </a>
         </div>
-        <div id="navbar-main" class="navbar-menu">
+        <div id="navbar-main" class="navbar-menu" style="">
             <div class="navbar-start" style="margin: 0 auto; width: 90%; flex-flow: row wrap; text-align: center; justify-content: space-around; ">
                 <router-link :to="{ name : 'about-us' }" class="navbar-item">
                     About
@@ -118,36 +118,32 @@ export default {
             }
         },
     },
-    created() {
+    mounted() {
+            // Get all "navbar-burger" elements
+            const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+            // Check if there are any navbar burgers
+            if ($navbarBurgers.length > 0) {
+
+                // Add a click event on each of them
+                $navbarBurgers.forEach( el => {
+                    el.addEventListener('click', () => {
+
+                        // Get the target from the "data-target" attribute
+                        const target = el.dataset.target;
+                        const $target = document.getElementById(target);
+
+                        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                        el.classList.toggle('is-active');
+                        $target.classList.toggle('is-active');
+
+                    });
+                });
+            }
     }
 
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-
-        // Add a click event on each of them
-        $navbarBurgers.forEach( el => {
-            el.addEventListener('click', () => {
-
-                // Get the target from the "data-target" attribute
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
-
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-
-            });
-        });
-    }
-
-});
 </script>
 
 <style scoped>
