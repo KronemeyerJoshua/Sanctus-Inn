4fd3c2a34c431b660245894e00fe927bb9b83c17
KronemeyerJoshua <jkroneme@asu.edu>
2020-06-19 12:17:58 -0700

    Made create function for threads

--- app/Http/Controllers/ForumThreadsController.php
+++ app/Http/Controllers/ForumThreadsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\forum_posts;
 use Illuminate\Http\Request;
 use App\forum_threads;
 
@@ -14,5 +15,25 @@ class ForumThreadsController extends Controller
     public function getNumberThreadsByCategory(Request $request, $subcategoryId)
     {
         return forum_threads::where('subcategory_id', $subcategoryId)->count();
+    }
+
+    public function create(Request $request)
+    {
+        $user = auth()->user();
+
+        // TODO: Check valid SQL Response
+        $thread = new forum_threads;
+        $thread->subcategory_id = $request->subcategory_id;
+        $thread->title = $request->title;
+        $thread->user_id = $user['id'];
+        $thread->save();
+
+        $post = new forum_posts;
+        $post->thread_id = $thread->id;
+        $post->user_id = $user['id'];
+        $post->content = $request->content;
+        $post->save();
+
+        return response()->json(['status' => '200', 'message' => 'Successfully posted']);
     }
 }
