61e983e9b32ed6d8624b263d4678bbf81569de06
KronemeyerJoshua <jkroneme@asu.edu>
2020-06-19 12:18:13 -0700

    Create thread modal created

---
+++ resources/js/components/forum/ThreadModal.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="modal">
+        <div class="modal-background" @click="close"></div>
+        <div class="modal-content">
+            <div class="form">
+            <input id="title" class="title" v-model="title" />
+            <Wysiwyg @button-clicked="submit"></Wysiwyg>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Wysiwyg from "./wysiwyg";
+    import { forum} from "../../services/EventService";
+    export default {
+        name: "ThreadModal",
+        components: {Wysiwyg},
+        data() {
+            return {
+                title: ""
+            }
+        },
+        props: {
+            subcategory_id: Number
+        },
+        methods: {
+            close() {
+                this.$emit('minimize-thread-modal')
+            },
+            submit(postData) {
+                forum.newThread({
+                    title: this.title,
+                    subcategory_id: 1,
+                    content: postData
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .modal-content {
+    }
+</style>
