70332f34aec8362d0dc4b885c01991480ba62fb8
KronemeyerJoshua <jkroneme@asu.edu>
2020-07-29 14:45:33 -0700

    extract packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix');
-
+const webpack = require('webpack');
+const path = require('path');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,5 +12,31 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.webpackConfig( {
+    plugins: [
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    ]
+});
+
+if (process.env.COMP_TYPE === 'css')
+    mix.sass('resources/sass/app.scss', 'public/css');
+else
+    mix.js('resources/js/app.js', 'public/js').extract(['tippy.js', 'any-base', '@fortawesome/fontawesome-svg-core', '@fortawesome/free-solid-svg-icons', '@fortawesome/vue-fontawesome']);
+
+
+if (mix.inProduction()) {
+    mix.version()
+        .options({
+            // Optimize JS minification process
+            terser: {
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }
+        });
+} else {
+    // Uses inline source-maps on development
+    mix.webpackConfig({
+        devtool: 'inline-source-map'
+    });
+}
