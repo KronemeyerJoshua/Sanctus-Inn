294c3ecbc282985bda19d167ad1eb1bef4b007dd
KronemeyerJoshua <jkroneme@asu.edu>
2020-07-31 17:33:18 -0700

    Added more specific error messages

--- resources/js/components/forum/ThreadModal.vue
+++ resources/js/components/forum/ThreadModal.vue
@@ -3,9 +3,9 @@
         <div class="modal-background" @click="close"></div>
         <div class="modal-content">
             <div class="form">
-                <label for="thread-title">Title</label><input id="thread-title" class="thread-title" v-model="title" />
+                <label for="thread-title">Title</label><input id="thread-title" class="thread-title" v-model="title" autocomplete="off" minlength="10" maxlength="100" />
                 <p v-if="error===429">It looks like you're doing that too quick. Try again in {{retry}}s</p>
-                <p v-if="error===500">Hm... Something went wrong.</p>
+                <p v-if="error===500">{{errorMessage}}</p>
             <Wysiwyg @button-clicked="validate"></Wysiwyg>
             </div>
         </div>
@@ -51,16 +51,20 @@
                 this.$emit('minimize-thread-modal')
             },
             validate(postData) {
-                let err = false;
-                if (postData.length < 5) {
-                    err = true;
-                    console.log("content too short");
+                this.error = 0;
+                if (postData.length < 200) {
+                    this.error = 500;
+                    this.errorMessage = "Your post body is " + (200-postData.length) + " characters too short."
                 }
-                if (this.title.length < 5) {
-                    err = true;
-                    console.log("title too short");
+                if (this.title.length < 10) {
+                    this.error = 500;
+                    this.errorMessage = "Your thread title is " + (10-this.title.length) + " characters too short."
                 }
-                if (!err) {
+                if (this.title.length > 100) {
+                    this.error = 500;
+                    this.errorMessage = "Your thread title is " + (this.title.length-100) + " characters too long."
+                }
+                if (this.error === 0) {
                     this.submit(postData)
                 }
             },
@@ -81,6 +85,7 @@
                             break;
                         default:
                             this.error = 500;
+                            this.errorMessage = "Hm... Something went wrong";
                             console.log(error.response);
                             break;
                     }
@@ -107,5 +112,10 @@
         background-color: var(--light-grey-bg);
         padding: 10px;
         border-radius: 5px;
+        width: 40%;
+        overflow: hidden;
+    }
+    .modal {
+        overflow: hidden;
     }
 </style>
