581738616686cdf76816d57775def1cd1f225a0c
KronemeyerJoshua <jkroneme@asu.edu>
2020-08-07 01:20:13 -0700

    New bbcode library for our wsyiwyg editor

---
+++ resources/js/services/BbcodeService.js
@@ -0,0 +1,50 @@
+let bbcode = {
+    bold: {
+        tagStart: "[b]",
+        tagEnd: "[/b]"
+    },
+    underline: {
+        tagStart: "[u]",
+        tagEnd: "[/u]"
+    },
+    italic: {
+        tagStart: "[i]",
+        tagEnd: "[/i]"
+    },
+    strikethrough: {
+        tagStart: "[del]",
+        tagEnd: "[/del]"
+    },
+    subscript: {
+        tagStart: "[sub]",
+        tagEnd: "[/sub]"
+    },
+    superscript: {
+        tagStart: "[sup]",
+        tagEnd: "[/sup]"
+    }
+};
+
+export function decode(content) {
+    let bbCodeMap = {
+        '\\[b\\](.+?)\\[/b\\]': '<strong>$1</strong>',
+        '\\[u\\](.+?)\\[/u\\]': '<u>$1</u>',
+        '\\[i\\](.+?)\\[/i\\]': '<i>$1</i>',
+        '\\[s\\](.+?)\\[/s\\]': '<del>$1</del>',
+        '\\[sub\\](.+?)\\[/sub\\]': '<sub>$1</sub>',
+        '\\[sup\\](.+?)\\[/sup\\]': '<sup>$1</sup>'
+    };
+
+    let bb = Object.keys(bbCodeMap).map((regex) => {
+        const replacement = bbCodeMap[regex];
+
+        return {
+            regexp: new RegExp(regex, 'igm'),
+            replacement: replacement
+        };
+    });
+
+    return bb.reduce((text, bbObj) => text.replace(bbObj.regexp, bbObj.replacement), content);
+}
+
+export default bbcode;
