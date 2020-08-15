aa2a9d67ecf458ad56b27b7caebb3c2dcddd56d8
KronemeyerJoshua <jkroneme@asu.edu>
2020-08-14 17:48:35 -0700

    Changed InsertLink Component into ImagePreview modal

---
+++ resources/js/components/wysiwyg/ImagePreview.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="modal">
+        <div class="modal-background" @click="close()"></div>
+        <div class="modal-content">
+            <img :src="linkProp">
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ImagePreview",
+        props: {
+            linkProp: {
+                type: [String],
+                default: ''
+            },
+        },
+        methods: {
+            close() {
+                this.$emit('close-preview-window');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .modal-content {
+        height: auto;
+        max-width: 80vh;
+    }
+</style>
