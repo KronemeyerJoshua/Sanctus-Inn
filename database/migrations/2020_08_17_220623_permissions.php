b7061026ba3f3b7cd0dd2894944683c976564319
KronemeyerJoshua <jkroneme@asu.edu>
2020-08-17 15:09:33 -0700

    Migration for permissions table

---
+++ database/migrations/2020_08_17_220623_permissions.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Permissions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->integer('id');
+            $table->text('role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
