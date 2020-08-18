89cc071a8fb9f7d0f30850300f3219cf0e601ee9
KronemeyerJoshua <jkroneme@asu.edu>
2020-08-17 22:02:39 -0700

    Added foreign key for permission_id

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
             $table->integer('rate_limit')->default(2);
+            $table->foreignId('permission_id')->default(1)->references('id')->on('permissions');
         });
     }
 
