3e3e230dce64a900e91e00f5958cb01d2c228d06
KronemeyerJoshua <jkroneme@asu.edu>
2020-08-21 04:55:54 -0700

    Updated Agree/Disagree button clicks and now loads users agree/disagrees

--- resources/js/components/forum/Post.vue
+++ resources/js/components/forum/Post.vue
@@ -6,20 +6,30 @@
             <div class="post-count"><slot name="post-count"></slot></div>
         </figure>
         <div class="media-content is-flex column" ref="media-content">
+
+                <!-- Post Content -->
                 <slot name="content" id="post-body" class="post-body"></slot>
+
+                <!-- Post Bottom Menu -->
                 <nav class="level is-flex" v-if="this.$store.state.auth.user">
+
+                    <!-- Post Bottom Left | Timestamp -->
                     <div class="level-left">
                         <slot name="timestamp"></slot>
                     </div>
+
+                    <!-- Post Bottom Right | Agree, Disagree, Quote, Report -->
                     <div class="level-right">
                         <a class="item" v-if="userId === userid">
                             <span class="icon is-small"><FontAwesomeIcon class="icon" icon="edit" title="Edit" /></span>
                         </a>
-                        <a class="item">
-                            <span class="icon is-small"><FontAwesomeIcon class="icon" icon="smile" title="Agree" />{{agree.length}}</span>
+                        <a class="item" @click="agreed">
+                            <span class="icon is-small"><FontAwesomeIcon :class="reactions.agree ? 'icon is-small agree' : 'icon is-small'" icon="smile" title="Agree" /></span>
+                            <p>{{agreeAmount}}</p>
                         </a>
-                        <a class="item">
-                            <span class="icon is-small"><FontAwesomeIcon class="icon" icon="frown" title="Disagree" />{{disagree.length}}</span>
+                        <a class="item" @click="disagreed">
+                            <span class="icon is-small"><FontAwesomeIcon :class="reactions.disagree ? 'icon is-small disagree' : 'icon is-small'" icon="frown" title="Disagree" /></span>
+                            <p>{{disagreeAmount}}</p>
                         </a>
                         <a class="item">
                             <span class="icon is-small"><FontAwesomeIcon class="icon" icon="quote-left" title="Quote" @click="this.scrollIntoView" /></span>
@@ -63,10 +73,61 @@
         },
         data() {
             return {
+                reactions: {
+                    agree: false,
+                    disagree: false
+                },
                 postBody: this.post,
+                agreeAmount: 0,
+                disagreeAmount: 0
             }
         },
         methods: {
+            /**
+             * Click event for agree icon
+             * TODO: Add proper SQL Update
+             */
+            agreed() {
+                if (this.reactions.agree)
+                {
+                    this.agreeAmount--;
+                    this.reactions.agree = false;
+                }
+                else if (this.reactions.disagree) {
+                    this.disagreeAmount--;
+                    this.reactions.disagree = false;
+                    this.agreeAmount++;
+                    this.reactions.agree = true;
+                }
+                else {
+                    this.agreeAmount++;
+                    this.reactions.agree = true;
+                }
+            },
+            /**
+             * Click event for disagree icon
+             * TODO: Add proper SQL Update
+             */
+            disagreed() {
+                if (this.reactions.disagree)
+                {
+                    this.disagreeAmount--;
+                    this.reactions.disagree = false;
+                }
+                else if (this.reactions.agree) {
+                    this.agreeAmount--;
+                    this.reactions.agree = false;
+                    this.disagreeAmount++;
+                    this.reactions.disagree = true;
+                }
+                else {
+                    this.disagreeAmount++;
+                    this.reactions.disagree = true;
+                }
+            },
+            /**
+             * Scrolls to the WYSIWYG Editor when quote button is clicked
+             */
             scrollIntoView() {
                 let editor = document.querySelector("#content-area");
                 let quote = document.querySelector("#post-" + this.postId);
@@ -74,6 +135,19 @@
                 console.log(quote.innerHTML);
                 this.$emit('quote-clicked', quote.innerHTML);
             }
+        },
+        created() {
+            // Find if user has any posts agreed/disagreed on this page
+            if (this.agree.find(o => o.id === this.userid)) {
+                this.reactions.agree = true;
+            }
+            else if (this.agree.find(o => o.id === this.userid)) {
+                this.reactions.disagree = true;
+            }
+
+            // Setup length to increment/decrement for user feedback later
+            this.agreeAmount = this.agree.length;
+            this.disagreeAmount = this.disagree.length;
         },
         mounted() {
             // Binds click events for each image in the post
@@ -88,6 +162,12 @@
 </script>
 
 <style scoped>
+    .agree {
+        color: var(--nav-text-color);
+    }
+    .disagree {
+        color: var(--nav-text-color);
+    }
     .level-left {
         justify-self: flex-start;
         align-self: flex-end;
